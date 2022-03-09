d85192d7d5a23b984c8d3e6b8abb292da0c70d9d
Andrew-Sem <avvyayvayva@gmail.com>
2022-03-09 19:56:48 +0300

    Added light-theme colors

--- pages/_document.js
+++ pages/_document.js
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en" data-theme="dark">
+    <Html lang="en" data-theme="light">
       <Head>
         <link
           href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600&display=swap"
