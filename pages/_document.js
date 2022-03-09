12503edea99349bb2800bd93ebcb8fddbf1c2bab
Andrew-Sem <avvyayvayva@gmail.com>
2022-03-09 14:02:55 +0300

    bug fix

--- pages/_document.js
+++ pages/_document.js
@@ -4,8 +4,6 @@ export default function Document() {
   return (
     <Html lang="en" data-theme="dark">
       <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link
           href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600&display=swap"
           rel="stylesheet"
