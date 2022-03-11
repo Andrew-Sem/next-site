44e41beb4c757977420c2036705a08988069444e
Andrew-Sem <avvyayvayva@gmail.com>
2022-03-11 12:11:13 +0300

    Get current theme from localStorage

--- components/ThemeSwitcher/ThemeSwitcher.js
+++ components/ThemeSwitcher/ThemeSwitcher.js
@@ -1,24 +1,29 @@
+import React, { useState, useEffect } from "react";
 import cl from "./ThemeSwitcher.module.css";
 
 export default function ThemeSwitcher() {
+  let theme;
+  useEffect(() => {
+    theme = window.localStorage.getItem("theme") || "light";
+    localStorage.setItem("theme", theme);
+
+    document.documentElement.setAttribute("data-theme", theme);
+  }, []);
+
   const toggleTheme = () => {
-    const theme = document.documentElement.getAttribute("data-theme");
-    if (theme === "light")
-      document.documentElement.setAttribute("data-theme", "dark");
-    else document.documentElement.setAttribute("data-theme", "light");
+    if (theme === "dark") {
+      theme = "light";
+      localStorage.setItem("theme", theme);
+    } else {
+      theme = "dark";
+      localStorage.setItem("theme", theme);
+    }
+    document.documentElement.setAttribute("data-theme", theme);
   };
 
   return (
-    // <div className={cl.themeSwitcher_wrapper}>
-    <div>
-      <input
-        className={cl.checkbox}
-        onChange={toggleTheme}
-        type="checkbox"
-        id="chekbox"
-      />
-      <span className={cl.checkbox_text}>Switch theme</span>
-    </div>
-    // </div>
+    <button className={cl.theme_switcher} onClick={toggleTheme}>
+      <span className={cl.theme_switcher__text}>Change theme</span>
+    </button>
   );
 }
