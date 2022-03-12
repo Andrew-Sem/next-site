ff9c7e2fe1a7bf29d16da702eb9b91710f8d407e
Andrew-Sem <avvyayvayva@gmail.com>
2022-03-12 15:28:12 +0300

    Added modal

--- components/UI/ModalBtn/ModalBtn.js
+++ components/UI/ModalBtn/ModalBtn.js
@@ -1,7 +1,7 @@
-import cl from "./AddBtn.module.css";
+import cl from "./ModalBtn.module.css";
 import { MdAdd } from "react-icons/md";
 
-export default function AddBtn({ props }) {
+export default function ModalBtn({ ...props }) {
   return (
     <div>
       <MdAdd className={cl.btn} {...props} />
--- pages/features.js
+++ pages/features.js
@@ -1,9 +1,19 @@
 import MainLayout from "../components/MainLayout/MainLayout";
+import ModalBtn from "../components/UI/ModalBtn/ModalBtn";
+import { useState } from "react";
+import Modal from "./../components/UI/Modal/Modal";
 
 export default function Features() {
+  const [modal, setModal] = useState(false);
+
   return (
     <MainLayout title="Features Page">
-      <div className="container"></div>
+      <div className="container">
+        <Modal visible={modal} setVisible={setModal}>
+          <h1>I'm Modal</h1>
+        </Modal>
+        <ModalBtn onClick={() => setModal(!modal)} />
+      </div>
     </MainLayout>
   );
 }
