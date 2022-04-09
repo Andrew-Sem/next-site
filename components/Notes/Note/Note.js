169940dd528264677c18431b46ec7a8cd8e688bc
Andrew-Sem <avvyayvayva@gmail.com>
2022-04-10 00:09:29 +0300

    add local db

--- components/Notes/Note/Note.js
+++ components/Notes/Note/Note.js
@@ -1,8 +1,9 @@
 import React from "react";
+import cl from "./Note.module.css";
 
 export default function Note({ title, body }) {
   return (
-    <div>
+    <div className={cl.note}>
       <h1>{title}</h1>
       <p>{body}</p>
     </div>
